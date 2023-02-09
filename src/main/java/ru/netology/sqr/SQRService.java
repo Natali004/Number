272fc00a6f70bb34fc96e2c87e8908e86f05ba02
unknown <natasha1764@mail.ru>
2023-02-09 11:49:09 +0300

    test

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,14 +2,12 @@ package ru.netology.sqr;
 
 public class SQRService {
 
-    public int calc(int low, int high)
-    {
+    public int calc(int low, int high) {
         int result = 0;
         for (int i = 10; i <= 99; i++) {
             int ii = i * i;
             if (ii >= low) {
-                if (ii <= high)
-                {
+                if (ii <= high) {
                     result++;
                 }
             }
